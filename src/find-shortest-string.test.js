1237df89eab19fe6e7685fd940bdceaffb6f8c30
Zac Gladman <zacharygladman@yahoo.co.uk>
2022-10-25 11:45:16 +0100

    Add changes to test file

--- src/find-shortest-string.test.js
+++ src/find-shortest-string.test.js
@@ -1,12 +1,10 @@
 const findShortestString = require("./find-shortest-string");
 
 test("findShortestString finds the shortest string in an array", () => {
-  expect(findShortestString(["hello, my name's Zac"])).toBe("my");
+  expect(findShortestString(["hello", "my", "name's", "Zac"])).toBe("my");
   expect(findShortestString(["Super", "Trouper"])).toBe("Super");
 });
 test("findShortestString finds the earliest string in cases of joint-shortest strings", () => {
   expect(findShortestString(["find", "shortest", "sing"])).toBe("find");
   expect(findShortestString(["!", "?"])).toBe("!");
 });
-
-//
